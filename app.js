19ead2460e78e18686d3caa8b8cd96929bf7d766
sk mahammad <114484239+sahidraza@users.noreply.github.com>
2023-06-20 16:29:30 +0530

    typescript

---
+++ app.js
@@ -0,0 +1,38 @@
+// Assignment operator
+
+// var x =5;
+// var y =10;
+// // console.log( "is both the x and y are equal"  + x == y);
+// console.log('is both the x and y are equal: ${x == y}');
+// comparison operator
+// var a = 30;
+// var b = 25;
+// console.log(a!=b)
+
+// logical operator
+
+// string concatenation operators
+
+// console.log("hello"+"world")
+
+// var myName = "SAHID"
+// console.log(myName +"  MAHAMMAD");
+
+// var tomr= "payment"
+// if (tomr=="not payment"){
+//     console.log("come to work");
+// }else{console.log("not come to site");}
+
+// conditional (ternary) operatore
+
+
+// var age= 19;
+// if(age< 18){
+//     console.log("are you eligible to vote")
+// } else{console.log("you are not eligible to vote")}
+
+// ternary operator
+
+// var age =16;
+// console.log((age => 18)?"you can vote":"you cant vote");
+
